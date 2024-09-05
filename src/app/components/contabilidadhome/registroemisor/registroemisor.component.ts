a0abadba3de311c34d7a3a1a93fdade59885f6fe
Melcocha <mr.arteaga06@gmail.com>
2024-09-04 23:50:11 -0600

    cambios

--- src/app/components/contabilidadhome/registroemisor/registroemisor.component.ts
+++ src/app/components/contabilidadhome/registroemisor/registroemisor.component.ts
@@ -1,8 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { RegistroToken } from 'src/app/pages/interfaces/registrotoken';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
+interface RegistroUsuario {
+  nombre: string;
+  apellidos: string;
+  codUsuario: string;
+  idTipoUsuario: number;
+  dui: string;
+  nit: string;
+  nrc: string;
+  correo: string;
+  password: string;
+}
+export interface UserData {
+  correo: string;
+  nombre: string;
+  apellido: string;
+  dui: string;
+}
 @Component({
   selector: 'app-registros',
   templateUrl: './registroemisor.component.html',
@@ -13,9 +32,16 @@ export class RegistroEmisorComponent implements OnInit {
   public opcion: number = 1;
   editable = false;
   seleccionado: any = null;
-  lista: RegistroToken[] = [];
-  tokenData: any = null;
+  lista: RegistroUsuario[] = [];
+  usuarioData: any = null;
+  public mostrar: number = 1;
+  public mostrarPassword: boolean = false;
 
+  displayedColumns: string[] = ['correo', 'nombre', 'apellido', 'dui'];
+  dataSource = new MatTableDataSource<UserData>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.formulario = this.fb.group({
       nombre: ['', Validators.required],
@@ -26,44 +52,59 @@ export class RegistroEmisorComponent implements OnInit {
     });
   }
   ngOnInit() {
-    
+    this.obtenerUsuarios();
   }
-  convertirAFormulario(usuario: RegistroToken) {
-    const fb = this.fb;
-    return fb.group({
-      id: fb.control(usuario.id, []),
-      tipo: fb.control(usuario.tipo, Validators.required),
-      token: fb.control(usuario.token, Validators.required),
-      creacion: fb.control(usuario.creacion, []),
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  obtenerUsuarios(): void {
+    const token = this.getCookie('token'); 
+    //const token = localStorage.getItem('token');
+    console.log(token); // Asumiendo que tienes una función getCookie
+    const query = `
+    query ObtenerUsuario {
+      obtenerPersona {
+        nombreComercial
+        nit
+        nrc
+        correoElectronico
+      }
+    }
+    `;
+    
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+  
+    this.http.post<any>('http://localhost:4000/', { query }, { headers }).subscribe({
+      next: result => {
+        const usuarioData = result.data.obtenerPersona;
+        console.log(usuarioData);
+        const lista = usuarioData.map((item: any) => ({
+          correo: item.nombreComercial,
+          nombre: item.nit,
+          apellido: item.nrc, 
+          dui: item.correoElectronico 
+        }));
+        this.dataSource.data = lista;
+        console.log(JSON.stringify(this.dataSource.data));  
+      },
+      error: error => {
+        console.error('Error fetching data:', error);
+      }
     });
   }
-  ingresarUsuario(): void {
-    const query = `
-      mutation CrearUsuario($parametros: usuarioInput) {
-        crearUsuario(parametros: $parametros) {
-          apellidos
-          codUsuario
-          correo
-          clave
-          nombre
-        }
-      }
-    `;
-    const variables = {
-      parametros: {
-        nombre: this.formulario.get('nombre')?.value,
-        apellidos: this.formulario.get('apellido')?.value,
-        correo: this.formulario.get('correo')?.value,
-        clave: this.formulario.get('password')?.value,
-        nrc: "",
-        codUsuario: this.formulario.get('codUsuario')?.value
-      }
-    };
-    this.http.post<any>('http://localhost:4000/', { query, variables }).subscribe(result => {
-      alert("Usuario Creado");
-    }); 
-
-  } 
+  getCookie(name: string): string | null {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
 }
 
 
